f44bbe4cb91126c2b17647e56992f4c177dd3c37
Tenshi-Sora <76926133+Tenshi-Sora@users.noreply.github.com>
2025-02-26 19:28:15 -0500

    Updated main.py so instead of drawing and updating objects individualy, it does it as a group

--- main.py
+++ main.py
@@ -11,13 +11,21 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}") #Tells the user the height of the screen
     clock = pygame.time.Clock() #Creates a clock to control the frame rate
     dt = 0 #Creates a variable to store the time since the last frame update
+    updatable = pygame.sprite.Group() #Creates a group to store all the objects that need to be updated
+    drawable = pygame.sprite.Group() #Creates a group to store all the objects that need to be drawn
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) #Creates a player object in the center of the screen
+    updatable.add(player) #Adds the player to the updatable group
+    drawable.add(player) #Adds the player to the drawable group
+
 
     while True: #Main game loop
         for event in pygame.event.get(): #Checks if the user has closed the window
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))  # Fills the screen with black background
+        updatable.update(dt)  # Updates all objects in the "updatable" group
+        for obj in drawable: # Draws all objects in the "drawable" group
+            obj.draw(screen)  
         player.update(dt) # Updates the player to allow for movement
         player.draw(screen) # Draws the player on the screen
         pygame.display.flip()  # Refresh screen
